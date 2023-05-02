1d5aa8da4a5f695c069c16ce878d0409f5219919
Tony Lampada <tonylampada@gmail.com>
2023-05-02 13:55:36 +0000

    test deploy

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Home = () => {
   return (
     <div>
-      <h1>Welcome to my app</h1>
+      <h1>Welcome to my appp</h1>
       <p>Go to the <Link to="/todo-list">Todo List</Link></p>
     </div>
   );
